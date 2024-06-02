c8215f24b2f5bddad28eee1a8f43aeae1993a072
Cordovacha <supremodota@gmail.com>
2024-06-02 11:12:39 -0400

    delete-svg-andCM

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #FBFBFB;
   min-height: 100vh;
+  background: #F2F2F2;
+
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,6 +18,8 @@ let selectitem = ref(null)
 //this delete all objet
 const confirm = (id) => {
   urlStore.deleteUrl(id);
+  open.value = false;
+
 };
 
 //this cancel dont work
@@ -40,24 +42,27 @@ const handleOk = (e) => {
 <template>
   <a-layout class="organizar">
     <a-layout-content style="padding: 0 50px; width: 100%">
+
       <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+
         <a-list>
+
           <img :src="selectitem.imagen" alt="" width="300"> {{ selectitem.title }} {{ selectitem.description }}
           <hr>
+
           <a-popconfirm title="estas seguro que quieres eliminar este video?" ok-text="Si" cancel-text="No"
-            @confirm="confirm(selectitem.id)" @cancel="cancel">
-            <a-button danger>Eliminar</a-button>
-          </a-popconfirm>
-          <hr>
+            @confirm="confirm(selectitem.id)" @cancel="cancel" >
+            <img src="../assets/delete.png" alt="" width="25">
+          </a-popconfirm> 
         </a-list>
       </a-modal>
 
       <!--  <img :src="item.imagen" alt="" width="200" /> -->
-      
+
       <form @submit.prevent="handleSubmit" class="margin">
         <h1>Agregar un video</h1>
         <a-input type="text" placeholder="Add Url" v-model:value="url" style="width: 500px" />
-        <a-button type="primary" html-type="submit">Agregar</a-button>
+        <a-button type="primary" html-type="submit" >Agregar</a-button>
       </form>
 
 
@@ -73,10 +78,11 @@ const handleOk = (e) => {
 </template>
 
 <style>
-.margin{
-margin-bottom: 5%;
-margin-top: 3%
+.margin {
+  margin-bottom: 5%;
+  margin-top: 3%
 }
+
 .organizar {
   display: flex;
   justify-content: flex-start;
